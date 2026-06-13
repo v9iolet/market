bd72a9abf62ea47cc5cebc9ee777b14caf9721c7
欧阳涛 <violet@ouyangtaodeMacBook-Air.local>
2026-06-13 23:10:46 +0800

    feat: add login page and routing

---
+++ market-frontend/pages/login/login.vue
@@ -0,0 +1,19 @@
+<template>
+  <view class="login-container">
+    <view class="header">
+      <text class="title">Login / Register</text>
+    </view>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<style lang="scss">
+.login-container {
+  min-height: 100vh;
+  background-color: $color-background;
+}
+</style>
