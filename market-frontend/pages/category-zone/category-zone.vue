17d9af0de5226471a6e36bc90fa069fe43ee5a8b
欧阳涛 <violet@ouyangtaodeMacBook-Air.local>
2026-06-13 16:04:23 +0800

    feat: scaffold category-zone page and route

---
+++ market-frontend/pages/category-zone/category-zone.vue
@@ -0,0 +1,26 @@
+<template>
+	<view class="category-zone-page">
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			categoryName: '全部分类'
+		}
+	},
+	onLoad(options) {
+		if (options.type) {
+			this.categoryName = options.type;
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.category-zone-page {
+	min-height: 100vh;
+	background-color: $color-background;
+}
+</style>
